44d6521641b7889a74d90dcb6c2a9b7e08250dec
Sahnawaz Hussain <sahnawazhussain852@gmail.com>
2022-11-06 22:35:22 +0530

    hello

--- crud&sorting_with_redux/src/Components/ProductCard.jsx
+++ crud&sorting_with_redux/src/Components/ProductCard.jsx
@@ -9,7 +9,6 @@ const ProductCard = ({
   price,
   handleDeleteProduct,
 }) => {
-  //console.log(props);
   return (
     <div data-cy={`product-card-${id}`}>
       <div data-cy="product-card-name">{name}</div>
@@ -25,11 +24,9 @@ const ProductCard = ({
       </div>
       <div data-cy="product-card-price">€ {price}</div>
       <div>
-        {/* Add a onClick handler for delete functionality */}
         <button onClick={() => handleDeleteProduct(id)} data-cy="delete-button">
           Delete Product
         </button>
-        {/* Link the Edit button to '/edit/:id' route, so that the user is navigate to the Edit page on button click */}
         <Link to={`/edit/${id}`}>
           <button data-cy="edit-button">Edit Product</button>
         </Link>
